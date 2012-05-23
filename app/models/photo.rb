bbe2861f147081234f53bedaef7cfa70c48171cc
Dmitry Lavrov <lavrovdv@gmail.com>
2012-05-23 10:04:42 +0400

    Исправлена модель photo.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible  :photo1, :photo2
-  has_attached_file :photo1, :processors => [:watermark], :styles => { :thumb => '250x250>', :resized => { :geometry => '500x500>',  :watermark_path => '#{RAILS_ROOT}/public/images/watermark.png', :position => 'Center' }}
+  has_attached_file :photo1 #, :processors => [:watermark], :styles => { :thumb => '250x250>', :resized => { :geometry => '500x500>',  :watermark_path => '#{RAILS_ROOT}/public/images/watermark.png', :position => 'Center' }}
   has_attached_file :photo2 #, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
